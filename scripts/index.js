593c06c4b07c79a54ef56f8914a9da646d4f03be
Daria Koshubarova <koshubarova.da@gmail.com>
2023-04-16 01:04:18 +0600

    Исправления

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 let popup = document.querySelector('.popup');
-let popupContainer = document.querySelector('.popup__container');
 let openPopupButton = document.querySelector('.profile__edit');
 let closePopupButton = document.querySelector('.popup__close');
 
@@ -8,8 +7,13 @@ openPopupButton.addEventListener('click', openPopup);
 closePopupButton.addEventListener('click',closePopup);
 
 function openPopup() {
+  let nameValue = document.querySelector('.profile__name').innerText;
+  let subtitleValue = document.querySelector('.profile__subtitle').innerText;
+  let nameValueOpen = document.getElementById('username-input');
+  let subtitleValueOpen = document.getElementById('description-input');
+  nameValueOpen.value = nameValue;
+  subtitleValueOpen.value = subtitleValue;
   popup.classList.add('popup_opened');
-
 };
 
 function closePopup() {
@@ -17,10 +21,15 @@ function closePopup() {
 };
 
 // Находим форму в DOM
-let formElement = document.querySelector('.popup__container');
+let formElement = document.querySelector('.popup__form');
 // Находим поля формы в DOM
-let nameInput = document.querySelector('.input-name');
-let jobInput = document.querySelector('.input-job');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
+
+let nameInputValue
+let jobInputValue
+let nameInputNew
+let jobInputNew
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -28,20 +37,19 @@ function handleFormSubmit (evt) {
     evt.preventDefault();// Эта строчка отменяет стандартную отправку формы.
 
 // Получите значение полей jobInput и nameInput из свойства value
-let nameInputValue = document.getElementById('username-input').value;
-let jobInputValue = document.getElementById('description-input').value;
+nameInputValue = document.getElementById('username-input').value;
+jobInputValue = document.getElementById('description-input').value;
 
 // Выберите элементы, куда должны быть вставлены значения полей
-let nameInputNew = document.querySelector('.profile__name');
-let jobInputNew = document.querySelector('.profile__subtitle');
+nameInputNew = document.querySelector('.profile__name');
+jobInputNew = document.querySelector('.profile__subtitle');
 
 // Вставьте новые значения с помощью textContent
 nameInputNew.textContent = nameInputValue;
 jobInputNew.textContent = jobInputValue;
 
 //Закрытие формы
-popup.classList.remove('popup_opened');
-
+closePopup();
 }
 
 // Прикрепляем обработчик к форме:
