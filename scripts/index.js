5e80065294dbc9322a8143da4071b34dc70cf3f8
Daria Koshubarova <koshubarova.da@gmail.com>
2023-04-17 08:11:29 +0600

    Исправление замечаний 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 let popup = document.querySelector('.popup');
 let openPopupButton = document.querySelector('.profile__edit');
 let closePopupButton = document.querySelector('.popup__close');
+let nameValue = document.querySelector('.profile__name');
+let subtitleValue = document.querySelector('.profile__subtitle');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
 
 openPopupButton.addEventListener('click', openPopup);
 
 closePopupButton.addEventListener('click',closePopup);
 
 function openPopup() {
-  let nameValue = document.querySelector('.profile__name').innerText;
-  let subtitleValue = document.querySelector('.profile__subtitle').innerText;
-  let nameValueOpen = document.getElementById('username-input');
-  let subtitleValueOpen = document.getElementById('description-input');
-  nameValueOpen.value = nameValue;
-  subtitleValueOpen.value = subtitleValue;
+  nameInput.value = nameValue.textContent;
+  jobInput.value = subtitleValue.textContent;
   popup.classList.add('popup_opened');
 };
 
@@ -22,31 +22,13 @@ function closePopup() {
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_job');
-
-let nameInputValue
-let jobInputValue
-let nameInputNew
-let jobInputNew
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function handleFormSubmit (evt) {
-    evt.preventDefault();// Эта строчка отменяет стандартную отправку формы.
-
-// Получите значение полей jobInput и nameInput из свойства value
-nameInputValue = document.getElementById('username-input').value;
-jobInputValue = document.getElementById('description-input').value;
-
-// Выберите элементы, куда должны быть вставлены значения полей
-nameInputNew = document.querySelector('.profile__name');
-jobInputNew = document.querySelector('.profile__subtitle');
-
-// Вставьте новые значения с помощью textContent
-nameInputNew.textContent = nameInputValue;
-jobInputNew.textContent = jobInputValue;
+  evt.preventDefault();// Эта строчка отменяет стандартную отправку формы.
+  nameValue.textContent = nameInput.value;
+  subtitleValue.textContent = jobInput.value;
 
 //Закрытие формы
 closePopup();
