878b51c008a180d141342b793d2147769dcb3497
Daria Koshubarova <koshubarova.da@gmail.com>
2023-06-25 08:20:58 +0600

    Вебпак

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import Card from '../components/Card.js';
-import { configValidation, initialCards } from '../utils/constants.js';
-import FormValidator from '../components/FormValidator.js';
-import Section from '../components/Section.js';
-import Popup from '../components/Popup.js';
-import PopupWithForm from '../components/PopupWithForm.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import UserInfo from '../components/UserInfo.js';
+import './pages/index.css';
+import Card from './components/Card.js';
+import { configValidation, initialCards } from './utils/constants.js';
+import FormValidator from './components/FormValidator.js';
+import Section from './components/Section.js';
+import Popup from './components/Popup.js';
+import PopupWithForm from './components/PopupWithForm.js';
+import PopupWithImage from './components/PopupWithImage.js';
+import UserInfo from './components/UserInfo.js';
 
 // попап редактирования профиля
 const popupEdit = document.querySelector('.popup-edit');
